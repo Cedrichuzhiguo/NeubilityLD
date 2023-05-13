6ac754b2bb08f67a395b4e4ad352db411dfea690
cedric <cedric@argoo.ca>
2023-05-13 09:31:36 -0400

    add a short echo

--- StartLDEditor.sh
+++ StartLDEditor.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #cd ~/Documents/LDE/LDE-v0.9.8.3.release.with.debug_OPP/
+echo "Start LDEditor in this folder: ~/Documents/LDE/LDE_current/"
 cd ~/Documents/LDE/LDE_current/
 ulimit -c unlimited
 ./LDEditor_original
