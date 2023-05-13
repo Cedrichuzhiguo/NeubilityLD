564b7f999f1d0b7c9cde6225500bbcd64f08acad
cedric <cedric@argoo.ca>
2023-05-12 21:55:13 -0400

    fix errors

--- LDEInstall.sh
+++ LDEInstall.sh
@@ -1,29 +1,30 @@
-
-LISCENSE_FOLDER = /home/label/Documents/Data/Newbility/LDEditor/license
-LDE_SRC = /home/label/Documents/Data/Newbility/LDEditor/
-LDE_TGT = ~/Documents/LDE/
-LDE_CURRENT = ${LDE_TGT}/LDE_current
-LDE_CURRENT_ZIP_FILENAME = LDE-v0.9.8.5.1.release.with.debug_OPP.tar.gz
+LISCENSE_FOLDER="/home/label/Documents/Data/Newbility/LDEditor/license"
+LDE_SRC="/home/label/Documents/Data/Newbility/LDEditor/"
+LDE_TGT="~/Documents/LDE"
+LDE_CURRENT="${LDE_TGT}/LDE_current"
+LDE_CURRENT_ZIP_FILENAME="LDE-v0.9.8.5.1.release.with.debug_OPP.tar.gz"
 
 # Find filename with .license extension from a subfolder
+licenseFile=$(${LDE_TGT} -maxdepth 1 -type f -name "*.license" | head -n 1)
+cp "${licenseFile}" "${LISCENSE_FOLDER}"
 
-licenseFile=$(find /path/to/folder -maxdepth 1 -type f -name "*.license" | head -n 1)
-cp ${licenseFile} ${LISCENSE_FOLDER}
+# Remove all the folder under LDE_TGT 
+rm -rf "${LDE_TGT}"/*
 
-
-#remove all the folder under LDE_TGT 
-rm -rf ${LDE_TGT}/*
-
-#unzip current zip file to LDE target folder
-gunzip -c ${LDE_SRC}/${LDE_CURRENT_ZIP_FILENAME} > ${LDE_TGT}
-dirname=$(find ${LDE_TGT} -maxdepth 1 -type d | head -n 1)
+# Unzip current zip file to LDE target folder
+gunzip -c "${LDE_SRC}/${LDE_CURRENT_ZIP_FILENAME}" > "${LDE_TGT}/${LDE_CURRENT_ZIP_FILENAME%.gz}"
+dirname=$(find "${LDE_TGT}" -maxdepth 1 -type d | head -n 1)
 
 if [ -n "$dirname" ]; then
-  #rename the dirname to LDE_CURRENT
-  mv dirname ${LDE_CURRENT}
+  # Rename the dirname to LDE_CURRENT
+  mv "$dirname" "${LDE_CURRENT}"
 fi
 
-#copy license file to the LDE_CURRENT folder
-cp ${licenseFile} ${LDE_CURRENT}/license.license
+# Copy license file to the LDE_CURRENT folder
+if [ ! -d "${LDE_CURRENT}" ]; then
+  mkdir "${LDE_CURRENT}"
+fi
+cp "${licenseFile}" "${LDE_CURRENT}/license.license"
+
 
 
